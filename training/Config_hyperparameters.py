004cd13ede3bf6ab029a9c706d29b8b2bf3f76be
unknown <andi.ramiqi@hesge.ch>
2024-08-15 11:50:35 +0200

    gitignore init

--- training/Config_hyperparameters.py
+++ training/Config_hyperparameters.py
@@ -13,19 +13,7 @@ DQN_CONFIG = {
     "seed": 1
 }
 
-DQN_CONFIG_BREAKOUT = {
-    "hidden_dim": 64,
-    "learning_rate": 0.00025,
-    "gamma": 0.99,
-    "batch_size": 32,
-    "replay_memory_size": 1000000,
-    "min_replay_memory_size": 50000,
-    "update_target_every": 10000,
-    "epsilon_start": 1.0,
-    "epsilon_decay": 0.000003,
-    "epsilon_min": 0.05,
-    "seed": 1
-}
+
 PPO_CONFIG = {
     "actor_hidden_dim": 64,
     "critic_hidden_dim": 64,
@@ -40,19 +28,7 @@ PPO_CONFIG = {
     "seed": 1
 }
 
-PPO_CONFIG_BREAKOUT = {
-    "actor_hidden_dim": 64,
-    "critic_hidden_dim": 64,
-    "actor_lr": 0.00005,
-    "critic_lr": 0.00005,
-    "gamma": 0.98,
-    "lambda_advantage": 0.95,
-    "clip_epsilon": 0.15,
-    "entropy_coeff": 0.01,
-    "epochs": 10,
-    "batch_size": 64,
-    "seed": 1
-}
+
 
 SAC_CONFIG = {
     "hidden_dim": 64,
@@ -63,6 +39,34 @@ SAC_CONFIG = {
     "gamma": 0.99,
     "tau": 0.005,
     "alpha": 0.2,
+    "batch_size": 64,
+    "seed": 1
+}
+
+DQN_CONFIG_BREAKOUT = {
+    "hidden_dim": 64,
+    "learning_rate": 0.00025,
+    "gamma": 0.99,
+    "batch_size": 32,
+    "replay_memory_size": 1000000,
+    "min_replay_memory_size": 50000,
+    "update_target_every": 10000,
+    "epsilon_start": 1.0,
+    "epsilon_decay": 0.000003,
+    "epsilon_min": 0.05,
+    "seed": 1
+}
+
+PPO_CONFIG_BREAKOUT = {
+    "actor_hidden_dim": 64,
+    "critic_hidden_dim": 64,
+    "actor_lr": 0.00005,
+    "critic_lr": 0.00005,
+    "gamma": 0.98,
+    "lambda_advantage": 0.95,
+    "clip_epsilon": 0.15,
+    "entropy_coeff": 0.01,
+    "epochs": 10,
     "batch_size": 64,
     "seed": 1
 }
